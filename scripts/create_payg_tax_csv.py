f22e2abb30f2a39fee9d43a5933cdfbbe887a2f0
brandonBarry <brandon96.barry@gmail.com>
2025-04-05 16:03:02 +0800

    cleaning

--- scripts/create_payg_tax_csv.py
+++ scripts/create_payg_tax_csv.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 
-from config.config import PROCESSED_DATA_DIR, BRACKET_SOURCE_INFO
+from config.config import BRACKET_SOURCE_INFO, PROCESSED_DATA_DIR
 from scripts.tax_bracket_calculator import calculate_tax_bracket_amount
 
 
-def create_payg_tax_csv():
+def create_income_statement():
 
     # initialize empty row structure
     rows = []
@@ -15,7 +15,7 @@ def create_payg_tax_csv():
     for source, (is_tax, is_cumulative) in BRACKET_SOURCE_INFO.items():
         row = {"name": source, "is_tax": is_tax, "amount": calculate_tax_bracket_amount(source, is_tax, is_cumulative)}
         rows.append(row)
-    payg_tax_df = pd.DataFrame(rows).sort_values(by="is_tax", ascending=False, inplace=True)
+    payg_tax_df = pd.DataFrame(rows).sort_values(by="is_tax", ascending=False)
 
     # save to repo
     output_path = f"{PROCESSED_DATA_DIR}/payg_tax.csv"
--- scripts/tax_bracket_calculator.py
+++ scripts/tax_bracket_calculator.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pandas as pd
 
-from config.config import BASE_SALARY, TAX_BRACKETS_FILE
+from config.config import BASE_SALARY, HEALTH_INSURANCE_COST, TAX_BRACKETS_FILE
 
 
 # calculate amount of tax or subsidy based on income brackets csv
@@ -14,7 +14,7 @@ def calculate_tax_bracket_amount(source: str, is_tax: bool, is_cumulative: bool)
     income_brackets["upper_bound"] = income_brackets["upper_bound"].replace("inf", np.inf).astype(float)
 
     # only take the parts of the file that are applicable to the current function call and ensure sorted by upper bound
-    income_brackets = income_brackets[income_brackets["source"] == source]
+    income_brackets = income_brackets[income_brackets["service"] == source]
     income_brackets.sort_values(by="upper_bound", inplace=True)
 
     # find dataframe index that salary is in
@@ -33,7 +33,10 @@ def calculate_tax_bracket_amount(source: str, is_tax: bool, is_cumulative: bool)
 
     # else, like hecs and medibank, it is a flat rate
     else:
-        amount = (BASE_SALARY) * (income_brackets.loc[bracket_row, "rate"])
+        if source == "medibank":
+            amount = (HEALTH_INSURANCE_COST) * (income_brackets.loc[bracket_row, "rate"])
+        elif source == "hecs":
+            amount = (BASE_SALARY) * (income_brackets.loc[bracket_row, "rate"])
 
     # finally, if the value is a tax, report it as a negative
     if is_tax:
