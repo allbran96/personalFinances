99eb8d827a8919ac807069393775aa69a9c8afc9
brandonBarry <brandon96.barry@gmail.com>
2025-04-05 16:03:02 +0800

    cleaning

--- config/config.py
+++ config/config.py
@@ -5,13 +5,14 @@ with open(Path("config/parameters.yml"), "r") as f:
     config = yaml.safe_load(f)
 
 # file paths
-TAX_BRACKETS_FILE = Path(config["paths"]["tax_brackets"])
+TAX_BRACKETS_FILE = Path(config["paths"]["tax_brackets_file"])
 RAW_DATA_DIR = Path(config["paths"]["raw_data"])
 PROCESSED_DATA_DIR = Path(config["paths"]["processed_data"])
 
 # numbers
-LEVY_RATE = config["tax_settings"]["levy_rate"]
-BASE_SALARY = config["immutables"]["base_salary"]
+LEVY_RATE = config["fixed_values"]["levy_rate"]
+BASE_SALARY = config["fixed_values"]["base_salary"]
+HEALTH_INSURANCE_COST = config["fixed_values"]["medibank_cost"]
 
 # information on tax brackets, like name (income, hecs, medibank), whether its a tax or not and whether its cumulatively added
 BRACKET_SOURCE_INFO = config["bracket_source_info"]
