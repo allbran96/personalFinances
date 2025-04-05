60af773fd4f6cc3f1309417b257a1addfa407676
brandonBarry <brandon96.barry@gmail.com>
2025-04-05 15:46:17 +0800

    added linting, type checking and collated tax_brackets

--- config/config.py
+++ config/config.py
@@ -1,7 +1,20 @@
 import yaml
 from pathlib import Path
 
-with open(Path('config/parameters.yml'), 'r') as f:
+with open(Path("config/parameters.yml"), "r") as f:
     config = yaml.safe_load(f)
 
-TAX_BRACKETS_DIR = Path(config["paths"]["tax_brackets"])
+# file paths
+TAX_BRACKETS_FILE = Path(config["paths"]["tax_brackets"])
+RAW_DATA_DIR = Path(config["paths"]["raw_data"])
+PROCESSED_DATA_DIR = Path(config["paths"]["processed_data"])
+
+# numbers
+LEVY_RATE = config["tax_settings"]["levy_rate"]
+BASE_SALARY = config["immutables"]["base_salary"]
+
+# information on tax brackets, like name (income, hecs, medibank), whether its a tax or not and whether its cumulatively added
+BRACKET_SOURCE_INFO = config["bracket_source_info"]
+
+# debugging
+DEBUG_MODE = config["other_settings"]["debug_mode"]
---
+++ main.py
@@ -0,0 +1,11 @@
+from scripts.create_payg_tax_csv import create_payg_tax_csv
+
+
+def main() -> None:
+
+    create_payg_tax_csv()
+    return
+
+
+if __name__ == "__main__":
+    main()
